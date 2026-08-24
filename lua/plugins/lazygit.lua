2cdc948d8f04eed2d2f0854cb635ce1035fe616a
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat lazygit.lua with stylua

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,16 +1,16 @@
 -- ~/.config/nvim/lua/plugins/lazygit.lua
 return {
-  'kdheepak/lazygit.nvim',
-  event = "InsertEnter",
+	"kdheepak/lazygit.nvim",
+	event = "InsertEnter",
 
-  dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = { "nvim-lua/plenary.nvim" },
 
-  config = function()
-    -- Map <leader>gg to open LazyGit
-    vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>', {
-      desc = ' Open LazyGit',
-      noremap = true,
-      silent = true,
-    })
-  end,
+	config = function()
+		-- Map <leader>gg to open LazyGit
+		vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", {
+			desc = " Open LazyGit",
+			noremap = true,
+			silent = true,
+		})
+	end,
 }

75d14c894caf4c49040a04044a9f2438218d821b
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat colorizer.lua with stylua

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,18 +1,18 @@
 return {
-  "NvChad/nvim-colorizer.lua",
-  event = { "BufReadPre", "BufNewFile" },
+	"NvChad/nvim-colorizer.lua",
+	event = { "BufReadPre", "BufNewFile" },
 
-  config = function()
-    local colorizer = require("colorizer")
+	config = function()
+		local colorizer = require("colorizer")
 
-    colorizer.setup({
-      filetypes = { "*" }, -- Highlight all filetypes
-      user_default_options = {
-        names = false,
-        RRGGBBAA = true,
-        rgb_fn = true,
-        hsl_fn = true,
-      },
-    })
-  end
+		colorizer.setup({
+			filetypes = { "*" }, -- Highlight all filetypes
+			user_default_options = {
+				names = false,
+				RRGGBBAA = true,
+				rgb_fn = true,
+				hsl_fn = true,
+			},
+		})
+	end,
 }

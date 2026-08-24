bf26b4d093d900f8d5ece29c130a92dd3f4df999
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat nvim-tree.lua with stylua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,39 +1,39 @@
 -- nvim-tree config & first-open toggle
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  keys = {
-    {
-      "<C-s>",
-      function()
-        require("utils.tree_toggle").toggle_at_cwd()
-      end,
-      desc = "Toggle File Tree at project cwd",
-    },
-  },
-  config = function()
-    local nvim_tree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"<C-s>",
+			function()
+				require("utils.tree_toggle").toggle_at_cwd()
+			end,
+			desc = "Toggle File Tree at project cwd",
+		},
+	},
+	config = function()
+		local nvim_tree = require("nvim-tree")
 
-    nvim_tree.setup {
-      view = { side = "left", width = 35 },
-      -- Keep the tree root following :cd so it shows the project the dock picker
-      -- opened (e.g. ~/dev/kos), not Neovim's startup cwd (~/dev).
-      sync_root_with_cwd = true,
-      respect_buf_cwd = true,
-      update_focused_file = { enable = true, update_root = true },
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-    }
-  end,
+		nvim_tree.setup({
+			view = { side = "left", width = 35 },
+			-- Keep the tree root following :cd so it shows the project the dock picker
+			-- opened (e.g. ~/dev/kos), not Neovim's startup cwd (~/dev).
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = { enable = true, update_root = true },
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+		})
+	end,
 }

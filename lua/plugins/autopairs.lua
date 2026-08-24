feff79f94f8260f1d65cd505ff6e0b948d6e8508
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat autopairs.lua with stylua

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -5,22 +5,22 @@
 --       Lua language server is not aware of the "cmp.event:on" API; it thinks "event" has no field.
 
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    local autopairs = require("nvim-autopairs")
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		local autopairs = require("nvim-autopairs")
 
-    autopairs.setup({
-      check_ts = true,
-    })
+		autopairs.setup({
+			check_ts = true,
+		})
 
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-    local cmp = require("cmp")
+		local cmp = require("cmp")
 
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }

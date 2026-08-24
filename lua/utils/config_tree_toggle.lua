204e8cea9855a2e60b29366e2b4f20f6c9c62736
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat config_tree_toggle.lua with stylua

--- lua/utils/config_tree_toggle.lua
+++ lua/utils/config_tree_toggle.lua
@@ -3,10 +3,10 @@ local mod = {}
 
 --- Toggle nvim-tree rooted at NVIM config directory
 function mod.toggle_at_config()
-  -- change cwd to ~/.config/nvim
-  vim.cmd('lcd ' .. vim.fn.stdpath('config'))
-  -- toggle the tree
-  require('nvim-tree.api').tree.toggle()
+	-- change cwd to ~/.config/nvim
+	vim.cmd("lcd " .. vim.fn.stdpath("config"))
+	-- toggle the tree
+	require("nvim-tree.api").tree.toggle()
 end
 
 return mod

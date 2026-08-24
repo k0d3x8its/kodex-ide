c47be1fa57c514a8fc200507e522b763e5c38749
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat autocmds.lua with stylua

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,17 +2,19 @@
 local mod = {}
 
 -- Treat Arduino .ino files as C++ for Treesitter & ALE
-vim.api.nvim_create_autocmd({'BufRead','BufNewFile'}, {
-  pattern = '*.ino',
-  callback = function() vim.bo.filetype = 'cpp' end,
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.ino",
+	callback = function()
+		vim.bo.filetype = "cpp"
+	end,
 })
 
 -- Prompt & rename current tab (used by Ctrl+k)
 function mod.rename_current_tab()
-  local name = vim.fn.input('Tab name: ')
-  if name ~= '' then
-    vim.cmd('file ' .. name)
-  end
+	local name = vim.fn.input("Tab name: ")
+	if name ~= "" then
+		vim.cmd("file " .. name)
+	end
 end
 
 return mod

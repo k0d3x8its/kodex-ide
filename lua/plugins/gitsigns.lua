8f87ec5291a65e21f1497038caed98a09fd7d02a
K0d3x <k0d3x@pm.me>
2026-08-24 13:15:16 -0400

    style: reformat gitsigns.lua with stylua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,10 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 
-  config = function()
-    local gitsigns = require("gitsigns")
+	config = function()
+		local gitsigns = require("gitsigns")
 
-    gitsigns.setup()
-  end
+		gitsigns.setup()
+	end,
 }
